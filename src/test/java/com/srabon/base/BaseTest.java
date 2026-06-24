47a6d8d66f8bdb225807e962b25a5de800d22db6
srabondebnath <srabondebnath82@gmail.com>
2026-06-23 21:12:16 -0400

    fix: use @AfterMethod so each test closes its own browser

--- src/test/java/com/srabon/base/BaseTest.java
+++ src/test/java/com/srabon/base/BaseTest.java
@@ -3,27 +3,29 @@ package com.srabon.base;
 import com.srabon.pages.LoginPage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 
 public class BaseTest {
 
     protected WebDriver driver;
-    protected LoginPage loginPage;
 
+    protected LoginPage loginPage;
 
     @BeforeMethod
     public void setUp(){
+        //This driverOne for testing login functionality with saucedemo
         driver=new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.saucedemo.com/");
-
-        //because login page is only valid after p
         loginPage=new LoginPage(driver);
+
+        //This driverTwo for testing fileUpload, ScreenShoot, DropDown functionality with formy
+
     }
 
-    @AfterClass
+    @AfterMethod
     public void tearDown(){
         if(driver!=null){
             driver.quit();
